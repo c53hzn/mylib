4d9863f30ccde8fdf200be582e0c64763927b2bf
Jenny <hzn2017@outlook.com>
2018-10-02 21:25:10 +0800

    change property name "title" to "_title"

--- monkey.ebaySearch.js
+++ monkey.ebaySearch.js
@@ -9,7 +9,6 @@
 // ==/UserScript==
 
 (function(){
-
 var aptx_css_str = "#nav-icon{\r\n\tposition: fixed;\r\n\ttop: 4px;\r\n\tleft: 4px;\r\n\twidth: 30px;\r\n\theight: 30px;\r\n\tfont-size: 24px;\r\n\tline-height: 30px;\r\n\ttext-align: center;\r\n\tborder-radius: 5px;\r\n\tbackground: white;\r\n\tbox-shadow: 0px 1px 2px gray;\r\n\tcursor: pointer;\r\n\tz-index: 99999;\r\n}\r\n#nav-mask{\r\n\tposition: fixed;\r\n\ttop: 0px;\r\n\tleft: 0px;\r\n\twidth: 100%;\r\n\theight: 100%;\r\n\tbackground: gray;\r\n\topacity: 0.5;\r\n\tz-index: 998;\r\n\tdisplay: none;\r\n}\r\n#navbar{\r\n\tposition: fixed;\r\n\ttop: 0px;\r\n\tleft: -240px;\r\n\twidth: 240px;\r\n\theight: 100%;\r\n\tborder-right: 2px solid silver;\r\n\tbackground: rgba(255,255,255,0.75);\r\n\tdisplay: flex;\r\n\tflex-direction: column;\r\n\tz-index: 999;\r\n}\r\n#navbar h2{\r\n\tmargin: 20px auto 10px 10px;\r\n}\r\n#aptx_txtarea{\r\n\tmargin: 10px auto;\r\n\twidth: 215px;\r\n\theight: 300px;\r\n\tresize: none;\r\n}\r\n#aptx_read{\r\n\tmargin: 0px auto 2px 10px;\r\n}\r\n#aptx_box{\r\n\tposition: fixed;\r\n\ttop: 0px;\r\n\tright: -300px;\r\n\tpadding-left: 10px;\r\n\twidth: 300px;\r\n\theight: 100%;\r\n\tbackground: rgba(255,255,255,0.75);\r\n\tz-index: 99999;\r\n\toverflow: hidden;\r\n}\r\n#navbar p, #aptx_box p{\r\n\tfont-size: 12px;\r\n}\r\n#navbar p{\r\n\tmargin: 2px auto 2px 10px;\r\n}\r\n#aptx_box p{\r\n\tmargin: 4px auto 4px 0px;\r\n}\r\n#navbar input[type=\"checkbox\"], #aptx_box input[type=\"checkbox\"]{\r\n\tmargin-left: 0px;\r\n}\r\n#aptx_table_div{\r\n\tmargin-top: 10px;\r\n\twidth: 100%;\r\n\theight: 356px;\r\n\toverflow: auto;\r\n}\r\n#aptx_table_div table{\r\n\tborder-collapse: collapse;\r\n}\r\n#aptx_table_div th, #aptx_table_div td{\r\n\tfont-size: 12px;\r\n\tborder: 1px solid black;\r\n}\r\n.aptx_searched{\r\n\tcolor: lightgreen;\r\n\tfont-weight: bold;\r\n\tbackground: rgba(0,0,0,0.5)\r\n}\r\n#navbar button, #aptx_box button{\r\n\tpadding: 1px 6px;\r\n\tborder: 2px outset buttonface;\r\n}\r\n#gh-bt{\r\n\tz-index: 99999;\r\n\tborder: 1px solid gray\r\n}";
 var aptx_DOM_str = "<div id=\"nav-icon\">\u2630<\/div>\r\n<div id=\"nav-mask\"><\/div>\r\n<nav id=\"navbar\">\r\n\t<header> \r\n\t\t<p>&nbsp;<\/p>\r\n\t\t<h2>Search in bulk<\/h2>\r\n\t\t<p>One product per line<\/p>\r\n\t<\/header>\r\n\t<textarea id=\"aptx_txtarea\"><\/textarea>\r\n\t<button id=\"aptx_read\">Start searching<\/button>\r\n\t<p>Data needed: <\/p>\r\n\t<p>\r\n\t\t<label for=\"aptx_sch_id\"><input type=\"checkbox\" id=\"aptx_sch_id\"\/>Item ID<\/label>\r\n\t\t<label for=\"aptx_sch_title\"><input type=\"checkbox\" id=\"aptx_sch_title\"\/>Title<\/label>\r\n\t\t<label for=\"aptx_sch_price\"><input type=\"checkbox\" id=\"aptx_sch_price\"\/>Price<\/label>\r\n\t\t<label for=\"aptx_sch_picURL\"><input type=\"checkbox\" id=\"aptx_sch_picURL\"\/>Pic URL<\/label>\r\n\t<\/p>\r\n\t<p>\r\n\t\t<label for=\"aptx_sch_picIMG\"><input type=\"checkbox\" id=\"aptx_sch_picIMG\"\/>Pic itself<\/label>\r\n\t<\/p>\r\n\t<p title=\"max 10 items\">\r\n\t\t<span style=\"cursor: default\">No. of items in result: <\/span>\r\n\t\t<input id=\"aptx_sch_num\" type=\"number\" min=\"1\" max=\"10\" value=\"1\"\/>\r\n\t<\/p>\r\n<\/nav>\r\n\r\n<div id=\"aptx_box\">\r\n\t<div id=\"aptx_table_div\">\r\n\t\t<table id=\"aptx_table\">\r\n\t\t\t<tbody>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<th width=\"280\" height=\"340\">Data area<\/th>\r\n\t\t\t\t<\/tr>\r\n\t\t\t<\/tbody>\r\n\t\t<\/table>\r\n\t<\/div>\r\n\t<p>\r\n\t\t<span style=\"font-size: 12px;\">Current index: <\/span>\r\n\t\t<input id=\"aptx_last_SKU_index\" disabled value=\"N\/A\" type=\"text\" style=\"width: 50px;\" \/>\r\n\t<\/p>\r\n\t<p>\r\n\t\t<label for=\"aptx_res_id\"><input disabled type=\"checkbox\" id=\"aptx_res_id\"\/>Item ID<\/label>\r\n\t\t<label for=\"aptx_res_title\"><input disabled type=\"checkbox\" id=\"aptx_res_title\"\/>Title<\/label>\r\n\t\t<label for=\"aptx_res_price\"><input disabled type=\"checkbox\" id=\"aptx_res_price\"\/>Price<\/label>\r\n\t\t<label for=\"aptx_res_picURL\"><input disabled type=\"checkbox\" id=\"aptx_res_picURL\"\/>Pic URL<\/label>\r\n\t\t<label for=\"aptx_res_picIMG\"><input disabled type=\"checkbox\" id=\"aptx_res_picIMG\"\/>Pic itself<\/label>\r\n\t<\/p>\r\n\t<p title=\"max 10 items\">\r\n\t\t<span style=\"cursor: default\">No. of items in result: <\/span>\r\n\t\t<input id=\"aptx_res_num\" style=\"width: 50px;\" disabled value=\"N\/A\"\/>\r\n\t<\/p>\r\n\t<p>\r\n\t\t<span style=\"font-size: 12px;\">Search index: <\/span>\r\n\t\t<input type=\"number\" id=\"aptx_SKU_index\" style=\"width: 50px;\" min=\"1\" value=\"1\"\/>\r\n\t<\/p>\r\n\t<p>\r\n\t\t<button id=\"aptx_search\">Search<\/button>\r\n\t\t<button id=\"aptx_fill\">Fill<\/button>\r\n\t\t<button id=\"aptx_copy\">copy table<\/button>\r\n\t<\/p>\r\n\t<\/p>\r\n\t<p>\r\n\t\t<button id=\"aptx_auto_search\">Auto search and fill<\/button>\r\n\t\t<button id=\"aptx_stop\">Stop<\/button>\r\n\t<\/p>\r\n<\/div>";
 $(document).ready(function() {
@@ -54,7 +53,7 @@ $(document).ready(function() {
 		if (search_profile.item_ID) {
 			$("#aptx_sch_id").prop("checked",true);
 		}
-		if (search_profile.title) {
+		if (search_profile._title) {
 			$("#aptx_sch_title").prop("checked",true);
 		}
 		if (search_profile.price) {
@@ -74,7 +73,7 @@ $(document).ready(function() {
 		if (export_profile.item_ID) {
 			$("#aptx_res_id").prop("checked",true);
 		}
-		if (export_profile.title) {
+		if (export_profile._title) {
 			$("#aptx_res_title").prop("checked",true);
 		}
 		if (export_profile.price) {
@@ -132,10 +131,10 @@ $(document).ready(function() {
 	});
 	$("#aptx_sch_title").on("change",function(){
 		if($("#aptx_sch_title").prop("checked") == true){
-			search_profile.title = true;
+			search_profile._title = true;
 			sessionStorage.setItem("search_profile",JSON.stringify(search_profile));
 		} else {
-			search_profile.title = false;
+			search_profile._title = false;
 			sessionStorage.setItem("search_profile",JSON.stringify(search_profile));
 		}
 	});
@@ -183,7 +182,7 @@ $(document).ready(function() {
 			} else {
 				$("#aptx_res_id").removeProp("checked");
 			}
-			if (export_profile.title) {
+			if (export_profile._title) {
 				$("#aptx_res_title").prop("checked",true);
 			} else {
 				$("#aptx_res_title").removeProp("checked");
@@ -210,7 +209,7 @@ $(document).ready(function() {
 				if(export_profile.item_ID){
 					th = th + "<th>item_ID_" + j + "</th>";
 				}
-				if(export_profile.title){
+				if(export_profile._title){
 					th = th + "<th>title_" + j + "</th>";
 				}
 				if(export_profile.price){
@@ -302,13 +301,11 @@ $(document).ready(function() {
 				}
 			}
 			var newURL = currURLarr.join("&");
-			
 			sessionStorage.setItem("last_SKU_index", last_SKU_index);
 			$("#aptx_last_SKU_index").val(last_SKU_index);
 			$("#aptx_table tr").eq(last_SKU_index).addClass("aptx_searched");
 			$("#aptx_table tr").eq(last_SKU_index).siblings().removeClass("aptx_searched");
-			//index++
-			
+			//index++	
 			if (SKU_index <= SKUlen) {
 				$("#aptx_SKU_index").val(SKU_index);
 				sessionStorage.setItem("SKU_index", SKU_index);
@@ -376,7 +373,7 @@ $(document).ready(function() {
 				if(export_profile.item_ID){
 					tds += "<td>" + itemId + "</td>";
 				}
-				if(export_profile.title){
+				if(export_profile._title){
 					tds += "<td>" + itemTitle + "</td>";
 				}
 				if(export_profile.price){
@@ -422,8 +419,9 @@ $(document).ready(function() {
 		var table_content = table_content3.replace(/\t<\/tr>/g, "</tr>");
 		sessionStorage.setItem("table_content", table_content);
 	}
+	/*解析SKU到表格、填充表格时需调用saveTable()*/
 
-	/*自动搜索SKU并填充到表格，间隔2秒执行下一个SKU - 开始*/
+	/*自动搜索SKU并填充到表格，间隔1秒执行下一个SKU - 开始*/
 	var search_timer;
 	$("#aptx_auto_search").click(function(){
 		//last searched SKU index
@@ -446,18 +444,18 @@ $(document).ready(function() {
 		search_timer = setTimeout(function(){
 			$("#aptx_fill").trigger("click");
 			$("#aptx_search").trigger("click");
-		},2000);
+		},1000);
 	}else if(isAutoSearchPresent && isSearchEnded){
 		setTimeout(function(){
 			$("#aptx_fill").trigger("click");
-		},2000);
+		},1000);
 		clearTimeout(search_timer);
 		sessionStorage.removeItem("aptx_auto_search");
 	}else if (isSearchEnded) {
 		clearTimeout(search_timer);
 		sessionStorage.removeItem("aptx_auto_search");
 	}
-	/*自动搜索SKU并填充到表格，间隔2秒执行下一个SKU - 结束*/
+	/*自动搜索SKU并填充到表格，间隔1秒执行下一个SKU - 结束*/
 
 	/*清除自动搜索的定时器开始*/
 	$("#aptx_stop").click(function(){
