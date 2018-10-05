78c9c3b80773ecd2add16a46fbbd21263e04b280
Jenny <hzn2017@outlook.com>
2018-10-05 14:09:53 +0800

    Update monkey.getTaskId.js

--- monkey.getTaskId.js
+++ monkey.getTaskId.js
@@ -10,54 +10,65 @@
 // ==/UserScript==
 
 (function(w) {
-w.getTaskId = function(key_word) {
-	var baseURL = window.location.href;
-	var API = baseURL + "/result?job_id=";
-	var taskIds = [];
-	var taskResults = $(".listingtable tr");
-	for (let i = 1; i < taskResults.length; i++) {
-		let temp = taskResults[i].children[1].innerText;
-		taskIds.push(temp);
-	}
-	var j = 1;
-	function iterateTasks(task_ID) {
-		if (j <= taskIds.length) {
-			$.ajax({
-				type: "GET",
-				url: API + task_ID,
-				dataType: "html",
-				success: function (data) {
-					var isRIDfound = false;
-					var div = document.createElement("div");
-					div.innerHTML = data;
-					var tds = div.getElementsByTagName("td");
-					for (let k = 0; k < tds.length; k++) {
-						let content = tds[k].innerText;
-						if (content.indexOf(key_word) != -1) {
-							isRIDfound = true;
-						}
-					}
-					if (isRIDfound) {
-						console.log(j + ": " + task_ID);
-					} else {
-						console.log(String(j));
-					}
-					j++;
-					iterateTasks (taskIds[j - 1]);
-				},
-				error: function (xhr, status, err) {
-					console.log(j + ": " + task_ID + "出错了，自己点开看吧");
-                    console.log(xhr);
-                    console.log(status);
-					console.log(err);
-					j++;
-					iterateTasks (taskIds[j - 1]);
-				}
-			});
-		} else {
-			return;
+	var isSearching = false;
+	w.getTaskId = function(key_word) {
+		var baseURL = window.location.href;
+		var API = baseURL + "/result?job_id=";
+		var taskIds = [];
+		var taskResults = $(".listingtable tr");
+		for (let i = 1; i < taskResults.length; i++) {
+			let temp = taskResults[i].children[1].innerText;
+			taskIds.push(temp);
 		}
+		var j = 1;
+		isSearching = true;
+		function iterateTasks(task_ID) {
+			if (j <= taskIds.length && isSearching) {
+				$.ajax({
+					type: "GET",
+					url: API + task_ID,
+					dataType: "html",
+					success: function (data) {
+						var isRIDfound = false;
+						var div = document.createElement("div");
+						div.innerHTML = data;
+						var tds = div.getElementsByTagName("td");
+						for (let k = 0; k < tds.length; k++) {
+							let content = tds[k].innerText;
+							if (content.indexOf(key_word) != -1) {
+								isRIDfound = true;
+							}
+						}
+						if (isRIDfound) {
+							console.log(j + ": " + task_ID);
+						} else {
+							console.log(String(j));
+						}
+						j++;
+						if (j > taskIds.length) {
+							isSearching = false;
+						}
+						iterateTasks(taskIds[j - 1]);
+					},
+					error: function (xhr, status, err) {
+						console.log(j + ": " + task_ID + "出错了，自己点开看吧");
+	                    console.log(xhr);
+	                    console.log(status);
+						console.log(err);
+						j++;
+						if (j > taskIds.length) {
+							isSearching = false;
+						}
+						iterateTasks(taskIds[j - 1]);
+					}
+				});
+			} else {
+				return;
+			}
+		}
+		iterateTasks(taskIds[j]);
 	}
-	iterateTasks(taskIds[j]);
-}
+	w.getTaskId_stop = function() {
+		isSearching = false;
+	}
 })(window);
